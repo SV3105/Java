b6d300a15997011a48451479b30edd6b6ec5f82f
SV3105 <snehavaghela3105@gmail.com>
2024-03-28 13:36:07 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,17 @@
+
+public class arrays {
+    public static void main(String[] args) {
+        //1.declaration
+//        int [] marks ;
+//        //2.memory allocation
+//        marks = new int[5];
+//        //3.Initialization
+//        marks[0] =100;
+        // this three things together
+        int [] marks = {99,200,30,32};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+    }
+}
